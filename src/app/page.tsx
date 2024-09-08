fb30fc5ca989b21bde67ff11fe9382c0ac5901e8
Alefe Bispo <alefe.bispo@onesight.com.br>
2024-09-08 12:04:26 -0300

    add couple images and use them at main page and banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
 import Banner from "~/components/Banner";
-import ConfirmPresenceSection from "~/components/ConfirmPresenceSection";
 import Counter from "~/components/Counter";
 import MobileNavBar from "~/components/MobileNavBar";
 import Header from "~/components/Header";
 import GiftsSection from "~/components/GiftsSection";
 import CeremonySection from "~/components/CeremonySection";
+import MessagesSection from "~/components/MessagesSection";
 
 export default function HomePage() {
   return (
@@ -14,8 +15,24 @@ export default function HomePage() {
       <Banner />
       <Counter />
       <CeremonySection />
+      <div className="section relative mb-20 h-[400px] md:h-[560px]">
+        <Image
+          src="/couple.jpg"
+          alt="Image principal do casal"
+          fill
+          className="h-full min-h-full w-full object-cover object-[70%_60%] align-middle md:object-[70%_70%] lg:object-[70%_60%]"
+        />
+      </div>
       <GiftsSection />
-      <ConfirmPresenceSection />
+      <div className="section relative mt-12 h-[400px] md:h-[560px]">
+        <Image
+          src="/couple_2.jpg"
+          alt="Image principal do casal"
+          fill
+          className="h-full min-h-full w-full object-cover object-[70%_60%] align-middle md:object-[70%_70%] lg:object-[70%_60%]"
+        />
+      </div>
+      <MessagesSection />
       <MobileNavBar />
     </main>
   );
